c0d994d547432e0ace64458fada6b1eb22aa5b8a
spline <reticulatingspline@github.com>
2014-10-12 07:12:07 -0400

    Add tests.

--- test.py
+++ test.py
@@ -9,6 +9,13 @@ from supybot.test import *
 
 class NBATestCase(PluginTestCase):
     plugins = ('NBA',)
+    
+    def testNBA(self):
+        self.assertNotError('nbachannel add #test')
+        self.assertNotError('nbachannel del #test')
+        self.assertError('nbaon')
+        self.assertError('nbaoff')
+    
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
